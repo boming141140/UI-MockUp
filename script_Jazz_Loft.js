cf084ea544ba399be417eaf9b75f6ffa0fbfa333
boming141140 <1269847618zzh@gmail.com>
2021-03-30 05:06:43 -0400

    first commit

---
+++ script_Jazz_Loft.js
@@ -0,0 +1,13 @@
+const getB = document.getElementById('main');
+const getM = document.getElementById('main-l');
+const getR = document.getElementById('main-r');
+getB.addEventListener('click', event => {
+    window.open("3D_Museuml.html");
+});
+getM.addEventListener('click', e => {
+    window.open('https://www.thejazzloft.org/')
+});
+
+getR.addEventListener('click', ev => {
+    window.open('https://you.stonybrook.edu/339wwu/homepage/')
+});
